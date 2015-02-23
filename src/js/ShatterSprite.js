f3e0cefeebf1de5696403580c9960ac9a2e2abb8
Cory Gugler <cory.gugler@gmail.com>
2015-02-22 22:46:45 -0500

    Refactor sprite group shattering

--- src/js/ShatterSprite.js
+++ src/js/ShatterSprite.js
@@ -1,27 +1,36 @@
 CrashLanding.Util.ShatterSprite = function ShatterSprite(game, key, frame) {
-    var sprite = game.add.sprite(-1000, -1000, key, frame);
-    var bmd = game.add.bitmapData(sprite.width, sprite.height);
-    bmd.draw(sprite, 0, 0, sprite.width, sprite.height);
-    bmd.update();
-    sprite = null;
-    var tempImage = new Image();
-    tempImage.src = bmd.canvas.toDataURL();
-    var iceShatter = new Shatter(tempImage, 12);
-    var shatteredGroup = game.add.group();
+    var numberOfSprites = 12;
+    var offCanvas = -1000;
 
-    iceShatter.images.forEach(function(el, ind, arr) {
-        var key = frame + ind;
-        game.cache.addImage(key, null, el.image);
-        var sprite = shatteredGroup.create(el.x, el.y, key);
-        sprite.originX = el.x;
-        sprite.originY = el.y;
-        game.physics.arcade.enable(sprite);
-        sprite.body.velocity.x = game.rnd.integerInRange(-50, 50);
-        sprite.body.velocity.y = game.rnd.integerInRange(-1000, -500);
-        sprite.body.checkCollision.left = false;
-        sprite.body.checkCollision.right = false;
-        sprite.body.checkCollision.up = false;
-    });
+    function getImageFromSprite(game, key, frame) {
+        var sprite = game.add.sprite(offCanvas, offCanvas, key, frame);
+        var bmd = game.add.bitmapData(sprite.width, sprite.height);
+        var image = new Image();
 
-    return shatteredGroup;
+        bmd.draw(sprite, 0, 0, sprite.width, sprite.height);
+        bmd.update();
+        sprite.destroy();
+        image.src = bmd.canvas.toDataURL();
+        return image;
+    }
+
+    function createShatteredGroup(game, shatteredImage) {
+        var shatteredGroup = game.add.group();
+
+        shatteredImage.images.forEach(function(image, ind, arr) {
+            var key = frame + ind;
+            game.cache.addImage(key, null, image.image);
+            var sprite = shatteredGroup.create(offCanvas, offCanvas, key);
+            sprite.originX = image.x;
+            sprite.originY = image.y;
+            game.physics.arcade.enable(sprite);
+            sprite.body.checkCollision.left = false;
+            sprite.body.checkCollision.right = false;
+            sprite.body.checkCollision.up = false;
+        });
+        return shatteredGroup;
+    }
+
+    return createShatteredGroup(game, 
+        new Shatter(getImageFromSprite(game, key, frame), numberOfSprites));
 }
